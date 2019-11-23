aebd76cd01d61c3e3b4bf951973e8dcb68c17cb5
denyadenya77 <jetropak77@gmail.com>
2019-11-23 13:15:31 +0200

    unit_test_2

--- unit_2/lesson_17/refactor/test_library/test_library_book.py
+++ unit_2/lesson_17/refactor/test_library/test_library_book.py
@@ -14,7 +14,12 @@ class TestBook(unittest.TestCase):
     #         self.book.count_of_lines()
     #         open_mock.assert_called()
 
-    # @mock.patch('__builtin__.open')
-    # def test_count_if_lines(self, open_mock):
-    #     self.book.count_of_lines()
-    #     open_mock.assert_called()
+    @mock.patch('unit_2.lesson_17.refactor.library.open')
+    def test_count_if_lines(self, open_mock):
+        open_mock.return_value = 'djflsj'
+        self.book.count_of_lines()
+        open_mock.assert_called()
+
+
+if __name__ == '__main__':
+    unittest.main()
